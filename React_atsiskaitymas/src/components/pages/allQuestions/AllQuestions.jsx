e76a40c550e32b602a5f2e74274cb1bebd0f29cd
OdetaDz <o.dziugyte@gmail.com>
2023-12-08 13:40:23 +0200

    Sort by date

--- React_atsiskaitymas/src/components/pages/allQuestions/AllQuestions.jsx
+++ React_atsiskaitymas/src/components/pages/allQuestions/AllQuestions.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import QuestionsContext from "../../../context/QuestionsContext"
 import OneQuestionCard from "../../UI/oneQuestionCard/OneQuestionCard";
 import styled from "styled-components";
 import UsersContext from "../../../context/UserContext";
+import AwnsersContext from "../../../context/AwnsersContext";
 import { Link } from "react-router-dom";
+
 
 const StyledQuetionsMain = styled.main`
     /* padding: 20px 40px; */
@@ -25,8 +27,10 @@ const StyledQuetionsMain = styled.main`
 const AllQuestions = () => {
 
     const { questions } = useContext(QuestionsContext);
-    const { loggedInUser } = useContext(UsersContext);
-   
+    const { loggedInUser, users } = useContext(UsersContext);
+    const { awnsers } = useContext(AwnsersContext);
+    // const commentNumber = awnsers.filter(awnser => awnser.questionId === data.id).length
+    const [ sortByDate, setSortByDate ] = useState(false);
 
     return ( 
         <StyledQuetionsMain>
@@ -36,8 +40,22 @@ const AllQuestions = () => {
                     <button>Ask a question</button>
                 </Link>
             }
+            <div>
+                <span>Sort:</span>
+                <button
+                    onClick={() => {setSortByDate(!sortByDate)}}
+                >By {sortByDate ? "oldest" : "newest"}</button>
+            </div>
             <div className="questions">
                 {
+                    sortByDate ? 
+                    questions.slice().sort((a, b) => new Date(a.created) - new Date(b.created)).map(question => (
+                        <OneQuestionCard
+                            key={question.id}
+                            data={question}
+                        />
+                    ))
+                    :
                     questions.map(question => {
                         return <OneQuestionCard
                             key = {question.id}
