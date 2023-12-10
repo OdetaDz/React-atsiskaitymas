cabd693229da96b2784af28d105069334213b491
OdetaDz <o.dziugyte@gmail.com>
2023-12-10 21:30:53 +0200

    pilnai sustilizuoti allQuestions ir questionCard

--- React_atsiskaitymas/src/components/UI/oneQuestionCard/OneQuestionCard.jsx
+++ React_atsiskaitymas/src/components/UI/oneQuestionCard/OneQuestionCard.jsx
@@ -7,37 +7,68 @@ import AwnsersContext from "../../../context/AwnsersContext";
 const StyledQuestionDiv = styled.div`
     display: grid;
     grid-template-columns: 1fr 5fr;
-    border-top: 1px solid #36363657;
+    border: 1px solid #36363657;
+    border-radius: 10px;
+    box-shadow: 1px 2px#36363657;
+    background-color: #8e9aaf20;
     width: 100%;
 
-    div.votes{
+    > div.votes{
         display: flex;
         flex-direction: column;
-        gap: 10px;
+        gap: 5px;
         align-items: center;
         margin-top: 20px;
+
+        > span{
+            font-size: 0.8rem;
+        }
+
+        > span:nth-child(1){
+            padding: 2px 5px;
+            border-radius: 5px;
+        }
+
+        > span:nth-child(3){
+            color: #8e9aaf;
+        }
     }
 
-    div.question{
+    > div > div.question{
         h1{
             font-size: 1.3rem;
             font-weight: 600;
+            font-family: 'Kalnia';
             
             a{
                 text-decoration: none;
                 color: black;
 
                 &:hover{
-                    color: pink;
+                    color: #8e9aaf;
                 }
             }
         }
     }
 
-    div.creator{
+    > div > div.creator{
         display: flex;
         gap: 10px;
         justify-content: flex-end;
+        align-items: center;
+        padding-bottom: 10px;
+
+        > span:nth-child(1){
+            font-size: 0.8rem;
+            color: #8e9aaf;
+        }
+
+        > span:nth-child(2){
+            font-size: 0.8rem;
+            color: #8e9aaf;
+            padding-right: 10px;
+            font-family: 'Kalnia'
+        }
     }
 `;
 
@@ -46,11 +77,14 @@ const OneQuestionCard = ({ data }) => {
     const { users } = useContext(UsersContext); 
     const { awnsers } = useContext(AwnsersContext);
     const skaicius = awnsers.filter(awnser => awnser.questionId === data.id).length;
+    const backgroundColor =
+    data.likes > 0 ? "#0080003b" : data.likes < 0 ? "#da1a1a3b" : "transparent";
+
     return ( 
         <StyledQuestionDiv>
             <div className="votes">
-                <span>votes: {data.likes}</span>
-                <span>comments: {skaicius}</span>
+                <span style={{ backgroundColor }}>Votes: {data.likes}</span>
+                <span>Comments: {skaicius}</span>
                 <span>Asked: {data.created}</span>
             </div>
             <div>
--- React_atsiskaitymas/src/components/pages/allQuestions/AllQuestions.jsx
+++ React_atsiskaitymas/src/components/pages/allQuestions/AllQuestions.jsx
@@ -7,19 +7,73 @@ import { Link } from "react-router-dom";
 
 
 const StyledQuetionsMain = styled.main`
-    /* padding: 20px 40px; */
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 5px;
     width: 100%;
+    background-color: #eee3e48b;
 
+    > h1{
+        font-family: 'Kalnia';
+        font-weight: 500;
+        margin-bottom: 7px;
+    }
+
+    > div.addNew{
+        width: 80%;
+        display: flex;
+        justify-content: end;
+
+        > span{
+            
+            > a{
+                text-decoration: none;
+                padding: 5px 10px;
+                border: 1px solid #39393936;
+                background-color: #cbc0d3a2;
+                border-radius: 10px;
+                color: black;
+                display: block;
+
+                &:hover{
+                    background-color: #8e9aaf9a;
+                }
+            }
+        }
+    }
+
+    > div.sort{
+        width: 80%;
+        justify-self: start;
+        margin-bottom: 7px;
+
+        > span{
+            padding-right: 15px;
+            font-family: 'Kalnia';
+            font-weight: 500;
+        }
+
+        > button{
+            border: 1px solid #39393936;
+            border-radius: 10px;
+            padding: 5px 10px;
+            background-color: #cbc0d3a2;
+
+            &:hover{
+                background-color: #8e9aaf9a;
+                cursor: pointer;
+            }
+        }
+    }
     > div.questions{
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 5px;
+        gap: 10px;
         width: 80%;
+        padding-bottom: 10px;
     }
 `;
 
@@ -33,11 +87,13 @@ const AllQuestions = () => {
         <StyledQuetionsMain>
             <h1>All Questions</h1>
             {
-                loggedInUser && <Link to="/questions/addNew">
-                    <button>Ask a question</button>
-                </Link>
+                loggedInUser && 
+                    <div className="addNew">
+                        <span><Link to="/questions/addNew">Ask a question</Link></span>
+                    </div>
+                
             }
-            <div>
+            <div className="sort">
                 <span>Sort by:</span>
                 <button
                     onClick={() => {setSortByDate(!sortByDate)}}
