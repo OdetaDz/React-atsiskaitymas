83a3c94f6a9bb7f9ca2b5b399685c2633bc292c2
OdetaDz <o.dziugyte@gmail.com>
2023-12-11 14:09:46 +0200

    error message element structure change

--- React_atsiskaitymas/src/components/UI/formikInput/FormikInput.jsx
+++ React_atsiskaitymas/src/components/UI/formikInput/FormikInput.jsx
@@ -1,20 +1,22 @@
 const FormikInput = ({ type, name, formik, placeholder }) => {
     return (
       <div>
-        <label htmlFor={name}>{name.charAt(0).toUpperCase() + name.slice(1)}:</label>
-        <input
+        <div>
+          <label htmlFor={name}>{name.charAt(0).toUpperCase() + name.slice(1)}:</label>
+          <input
           type={type}
           name={name} id={name}
           value={formik.values[name]} // values['hihi'] === values.hihi 
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           placeholder={placeholder? placeholder:''}
-        />
+          />
+        </div>
         {
           formik.touched[name] && formik.errors[name] &&
-          <p
-            style={{ color:"red" }}
-          >{formik.errors[name]}</p>
+          <div>
+              <p style={{ color:"red" }}>{formik.errors[name]}</p>
+          </div>
         }
       </div>
     );
